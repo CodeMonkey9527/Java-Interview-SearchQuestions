9c58524b0b31d6bcb0123de81a6db2a0958ba893
CodeMonkey9527 <frank.shishen@gmail.com>
2013-04-04 09:17:58 -0700

    add finding words in a dicition

--- src/frank/interview/search/questions/FindWordsFromLetterGrid.java
+++ src/frank/interview/search/questions/FindWordsFromLetterGrid.java
@@ -1,15 +1,129 @@
 package frank.interview.search.questions;
 
-public class FindWordsFromLetterGrid {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
-	public static boolean findWord(char [][] grid, int n, String word) {
+import frank.interview.ds.trie.Node;
+import frank.interview.ds.trie.Trie;
+
+/**
+ * Given a grid of random letters, and a target word, find a path in the grid,
+ * which corresponds to the target word. 
+ * 
+ * Given a grid of random letters, and a list of valid words, find all possible paths
+ * that corresponds to the valid words
+ * @author Frank
+ *
+ */
+public class FindWordsFromLetterGrid {
+	
+	private static Trie<String> buildDictionary(String file) throws Exception {
+		Trie<String> dict = new Trie<String>();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				String word = line.trim();
+				if (word.matches("[a-z|A-Z]+") && word.length() > 1) {
+					dict.put(word, word);
+				}
+			}
+			return dict;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static Set<String> findWords(char [][] grid, int n, String dictFile) throws Exception {
 		printLetterGrid(grid, n);
+		Trie<String> dict = buildDictionary(dictFile);
+		boolean [][] taken = new boolean [n][n];
+		List<String> wordsFound = new LinkedList<String>();
+		Set<String> result = new TreeSet<String>();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				List<String> found = explore(grid, n, i, j, dict.getRoot(), taken);
+				if (found != null) {
+					wordsFound.addAll(found);
+				}
+			}
+		}
+		result.addAll(wordsFound);
+		return result;
+	}
+	
+	private static List<String> explore(char [][] grid, int n, int i, int j, Node<String> node, boolean [][] taken) {
+		if (node == null) {
+			return null;
+		}
+		List<String> result = new LinkedList<String>();
+		if (!taken[i][j]) {
+			Node<String> next = node.getChild(grid[i][j]);
+			if (next != null) {
+				if (next.getValue() != null) {
+					result.add(next.getValue());
+				}
+				List<String> found = null;
+				taken[i][j] = true;
+				// search in upper cell
+				if (i > 0) {
+					found = explore(grid, n, i-1, j, next, taken);
+					if (found != null) result.addAll(found);
+				}
+				// search in right cell
+				if (j < n-1) {
+					found = explore(grid, n, i, j+1, next, taken);
+					if (found != null) result.addAll(found);
+				}
+				// search in the lower cell
+				if (i < n-1) {
+					found = explore(grid, n, i+1, j, next, taken);
+					if (found != null) result.addAll(found);
+				}
+				// search in the left cell
+				if (j > 0) {
+					found = explore(grid, n, i, j-1, next, taken);
+					if (found != null) result.addAll(found);
+				}
+				taken[i][j] = false;
+				return result;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	private static boolean [][] initTakenMatrix(int n) {
 		boolean [][] taken = new boolean [n][n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				taken[i][j] = false;
 			}
 		}
+		return taken;
+	}
+	
+	public static boolean findWord(char [][] grid, int n, String word) {
+		printLetterGrid(grid, n);
+		boolean [][] taken = initTakenMatrix(n);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (findWord(grid, n, word, i, j, 0, taken)) {
--- tst/frank/interview/search/questions/test/FindWordsFromLetterGridTest.java
+++ tst/frank/interview/search/questions/test/FindWordsFromLetterGridTest.java
@@ -2,6 +2,8 @@ package frank.interview.search.questions.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,4 +35,29 @@ public class FindWordsFromLetterGridTest {
 		assertFalse(FindWordsFromLetterGrid.findWord(grid, 5, "dinmqvu"));
 	}
 
+	@Test
+	public void testFindWords1() throws Exception {
+		char [][] grid = {
+			{'a', 'b', 'c', 'd', 'e'}, 
+			{'f', 'g', 'h', 'i', 'j'}, 
+			{'k', 'l', 'm', 'n', 'o'}, 
+			{'p', 'q', 'r', 's', 't'}, 
+			{'u', 'v', 'w', 'x', 'y'}
+		};
+		System.out.println(FindWordsFromLetterGrid.findWords(grid, 5, "data/dict.txt"));
+	}
+	
+	@Test
+	public void testFindWordsRandom() throws Exception {
+		int n = 5;
+		char [][] grid = new char [n][n];
+		Random rn = new Random();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				char ch = (char)(rn.nextInt(26)+'a');
+				grid[i][j] = ch;
+			}
+		}
+		System.out.println(FindWordsFromLetterGrid.findWords(grid, n, "data/dict.txt"));
+	}
 }
